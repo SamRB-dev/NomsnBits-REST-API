bcc928d3fd371d1464bd080414f21600e3e64fa6
Fr34K008 <sam_freak008@protonmail.com>
2023-02-12 13:16:08 +0600

    API call - Retrieving data

---
+++ db/DbConfig.js
@@ -0,0 +1,41 @@
+class DBHandler {
+  #DB = "nomsnbits";
+  #DB_USER = "root";
+  #DB_PASSWD = "try_h4ck_me_b1tch";
+  #DB_URL = "127.0.0.1";
+
+  #MainHandler = null;
+
+  constructor() {
+    this.handler = require("mysql");
+  }
+
+  #CreateConnection() {
+    let handler = this.handler.createConnection({
+      host: this.#DB_URL,
+      user: this.#DB_USER,
+      password: this.#DB_PASSWD,
+      database: this.#DB,
+    });
+
+    this.#MainHandler = handler;
+  }
+
+  #DBConnect() {
+    let handler = this.#MainHandler;
+    handler.connect((error) => {
+        if (error) throw error.message;
+    });
+    console.log("Connected");
+  }
+
+  GetMainHandler(){
+    this.#CreateConnection();
+    this.#DBConnect();
+    return this.#MainHandler;
+  }
+}
+
+// Instantiation
+let handler = new DBHandler()
+module.exports = handler.GetMainHandler();
---
+++ routes/api.js
@@ -0,0 +1,25 @@
+// Imports
+const express = require("express");
+const router = express.Router();
+const db = require("../db/DbConfig");
+
+// Handling Request - Functional Approach
+router.get("/", async (request, response) => {
+  await response.json({
+    status: 200,
+    message: "Users",
+  });
+});
+
+router.get("/users", async (request, response) => {
+  await db.query("SELECT * FROM users", (error,data) => {
+    if (error) throw error.message;
+    response.json({
+      status:200,
+      data:data
+    });
+  });
+});
+
+// Exporting the router
+module.exports = router;
