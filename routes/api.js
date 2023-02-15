68ec938124198a5549561bf15c8a9cca75d410d7
Fr34K008 <sam_freak008@protonmail.com>
2023-02-16 02:08:27 +0600

    API - /register

--- routes/api.js
+++ routes/api.js
@@ -7,18 +7,51 @@ const db = require("../db/DbConfig");
 router.get("/", async (request, response) => {
   await response.json({
     status: 200,
-    message: "Users",
+    message: "OK",
   });
 });
 
+router.post("/register", async (request, response) => {
+  // const { uname, email, passwd, date } = request.body;
+  let uname = request.body.username;
+  let email = request.body.email;
+  let passwd = request.body.password;
+  let date = request.body.regDate;
+  await db.query(
+    `INSERT INTO users (username, email, password, registration_date) VALUES ('${uname}', '${email}', '${passwd}', '${date}');`,
+    (error, dataResponse) => {
+      if (error) {
+        console.log(error.message);
+        response.json({
+          status: 500,
+          message: "Internal Server Error",
+        });
+      }
+
+      response.json({
+        status: 200,
+        message: "success",
+      });
+    }
+  );
+});
+
 router.get("/users", async (request, response) => {
-  await db.query("SELECT * FROM users", (error, data) => {
-    if (error) throw error.message;
-    response.json({
-      status: 200,
-      data: data,
-    });
-  });
+  await db.query(
+    "SELECT user_id, username, email, password, registration_date FROM users",
+    (error, data) => {
+      if (error) {
+        response.json({
+          status: 500,
+          message: "Internal Server Error",
+        });
+      }
+      response.json({
+        status: 200,
+        data: data,
+      });
+    }
+  );
 });
 
 // Exporting the router
